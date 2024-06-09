b87469aa3604692339c80820ac63fd65d6223c89
문효안 <hyoanmoon@munhyoan-ui-MacBookAir.local>
2024-06-09 23:04:57 +0900

    implement add items to cart function

--- src/action/cartAction.js
+++ src/action/cartAction.js
@@ -1,15 +1,29 @@
 import api from "../utils/api";
 import * as types from "../constants/cart.constants";
 import { commonUiActions } from "../action/commonUiAction";
+import { type } from "@testing-library/user-event/dist/type";
 const addToCart =
   ({ id, size }) =>
-  async (dispatch) => {};
+    async (dispatch) => {
+      try {
+        dispatch({ type: types.ADD_TO_CART_REQUEST })
+        const response = await api.post("/cart", { productId: id, size, qty: 1 }); //id, 사이즈, 수량 (기본으로 1개)를 백엔드로 보낸다.
+        if (response.status !== 200) throw new Error(response.error);
+        dispatch({ type: types.ADD_TO_CART_SUCCESS, payload: response.data.cartItemQty });
+        console.log("rrrrr",response)
+        dispatch(commonUiActions.showToastMessage("Add to cart", "success"));
 
-const getCartList = () => async (dispatch) => {};
-const deleteCartItem = (id) => async (dispatch) => {};
+      } catch (error) {
+        dispatch({ type: types.ADD_TO_CART_FAIL, payload: error.message });
+        dispatch(commonUiActions.showToastMessage(error.message, "error"));
+      }
+    };
 
-const updateQty = (id, value) => async (dispatch) => {};
-const getCartQty = () => async (dispatch) => {};
+const getCartList = () => async (dispatch) => { };
+const deleteCartItem = (id) => async (dispatch) => { };
+
+const updateQty = (id, value) => async (dispatch) => { };
+const getCartQty = () => async (dispatch) => { };
 export const cartActions = {
   addToCart,
   getCartList,
--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -5,33 +5,47 @@ import { useDispatch, useSelector } from "react-redux";
 import { productActions } from "../action/productAction";
 import { currencyFormat } from "../utils/number";
 import "../style/productDetail.style.css";
+import { cartActions } from "../action/cartAction";
 
-const ProductDetail = () => {
+  const ProductDetail = () => {
   const dispatch = useDispatch();
-  const [size, setSize] = useState("");
   const { id } = useParams();
   const [sizeError, setSizeError] = useState(false);
   const navigate = useNavigate();
   const productDetail = useSelector((state) => state.product.productDetail); // 상태 가져오기
+  const [size, setSize] = useState("");
+  const {user} = useSelector((state) => state.user);
 
+  // ∙상품 디테일 정보 가져오기
   useEffect(() => {
-    // 상품 디테일 정보 가져오기
-    dispatch(productActions.getProductDetail(id));
+      dispatch(productActions.getProductDetail(id));
   }, [dispatch, id]);
 
-  const addItemToCart = () => {
-    // 사이즈를 아직 선택안했다면 에러
-    // 아직 로그인을 안한유저라면 로그인페이지로
-    // 카트에 아이템 추가하기
-  };
-
+  // ∙사이즈 추가하기
   const selectSize = (value) => {
-    // 사이즈 추가하기
     setSize(value);
-    setSizeError(false);
+    // 사이즈 에러가 true면 에러를 false
+    if(sizeError) setSizeError(false);
+    // console.log(value) => 누르는 대로 s, m, l...
   };
 
-  // productDetail이 null이거나 undefined인 경우
+  // ∙카드에 추가 버튼
+  const addItemToCart = () => {
+    // 1.사이즈를 아직 선택안했다면 에러
+    if( size === ""){
+      setSizeError(true)
+      return
+    }
+    // 2. 아직 로그인을 안한유저라면 로그인페이지로
+    if(!user) {
+      alert("로그인이 필요합니다")
+      navigate("/login")
+    };
+    // 3. 카트에 아이템 추가하기
+    dispatch(cartActions.addToCart({ id,size }))
+  };
+
+  // ❕productDetail이 null이거나 undefined인 경우
   if (!productDetail) {
     return <div>Loading...</div>;
   }
